1a3f129836dcaedf78dd001f9f396ffe19215b20
Farrel <farrelathallah06@gmail.com>
2024-08-04 13:53:33 +0800

    Revision Commit

--- web.py
+++ web.py
@@ -16,9 +16,9 @@ for index, todo in enumerate(todos):
     if checkbox:
         todos.pop(index)
         function.write_todos(todos)
-        del st.session_state[todo]
+        st.legacy_caching.clear_cache(todo)
         st.experimental_rerun()
 
-st.text_input(label="", placeholder="Enter a todo...",
+st.text_input(label="Enter a todo", placeholder="Enter a todo...",
               on_change=add_todo, key='new_todo')
 
